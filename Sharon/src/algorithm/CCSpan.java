7a432fab69b2e16547dc83252a63fc1b4b589d7b
opoppe <olga.poppe@gmail.com>
2017-01-26 14:45:19 -0500

    Initial commit

---
+++ Sharon/src/algorithm/CCSpan.java
@@ -0,0 +1,10 @@
+package algorithm;
+
+public class CCSpan {
+	
+	 public static void main(String[] args) {
+	     // Prints "Hello, World" to the terminal window.
+		 System.out.println("Hello, World");
+	 }
+
+}
