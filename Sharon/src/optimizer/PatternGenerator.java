f9a92ec8807ae16f990400e6d611f9e765c39261
opoppe <olga.poppe@gmail.com>
2017-01-26 16:35:22 -0500

    Reading of paramenters in pattern generator.

--- Sharon/src/optimizer/PatternGenerator.java
+++ Sharon/src/optimizer/PatternGenerator.java
@@ -3,8 +3,25 @@ package optimizer;
 public class PatternGenerator {
 	
 	 public static void main(String[] args) {
-	     // Prints "Hello, World" to the terminal window.
-		 System.out.println("Hello, World");
-	 }
+		 
+		 /*** Read and print the input parameters ***/
+		 int number_of_patterns = 0;
+		 int min_length = 0;
+		 int max_length = 0;
+		 int number_of_event_types = 0;	    
+		 
+		 for (int i=0; i<args.length; i++) {
+			if (args[i].equals("-number_of_patterns")) 		number_of_patterns = Integer.parseInt(args[++i]);
+			if (args[i].equals("-min_length")) 				min_length = Integer.parseInt(args[++i]);
+			if (args[i].equals("-max_length")) 				max_length = Integer.parseInt(args[++i]);
+			if (args[i].equals("-number_of_event_types")) 	number_of_event_types = Integer.parseInt(args[++i]);
+		 }	
+				
+		 System.out.println(	"Number of patterns: " + number_of_patterns +
+			   "\nMin length: " + min_length +
+			   "\nMax length: " + max_length +
+			   "\nNumber of event types: " + number_of_event_types +
+				"\n----------------------------------");	    
+		}
 
 }
