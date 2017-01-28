0d8a2bebdead1cb6dc5e45e062b9e1e788a945f1
olgapoppe <olga.poppe@gmail.com>
2017-01-27 21:49:25 -0500

    Pattern generator is done.

--- Sharon/src/optimizer/PatternGenerator.java
+++ Sharon/src/optimizer/PatternGenerator.java
@@ -1,27 +1,33 @@
 package optimizer;
 
+import java.util.Random;
+
 public class PatternGenerator {
 	
 	 public static void main(String[] args) {
 		 
 		 /*** Read and print the input parameters ***/
-		 int number_of_patterns = 0;
-		 int min_length = 0;
-		 int max_length = 0;
-		 int number_of_event_types = 0;	    
+		 int k = 0;
+		 int l = 0;
+		 int t = 0;	    
 		 
 		 for (int i=0; i<args.length; i++) {
-			if (args[i].equals("-number_of_patterns")) 		number_of_patterns = Integer.parseInt(args[++i]);
-			if (args[i].equals("-min_length")) 				min_length = Integer.parseInt(args[++i]);
-			if (args[i].equals("-max_length")) 				max_length = Integer.parseInt(args[++i]);
-			if (args[i].equals("-number_of_event_types")) 	number_of_event_types = Integer.parseInt(args[++i]);
+			if (args[i].equals("-k"))	k = Integer.parseInt(args[++i]);
+			if (args[i].equals("-l"))	l = Integer.parseInt(args[++i]);
+			if (args[i].equals("-t")) 	t = Integer.parseInt(args[++i]);
 		 }	
 				
-		 System.out.println(	"Number of patterns: " + number_of_patterns +
-				 "\nMin length: " + min_length +
-			   	 "\nMax length: " + max_length +
-			   	 "\nNumber of event types: " + number_of_event_types +
-				 "\n----------------------------------");	    
-	 	 }	
-
+		 System.out.println(k + " random patterns of length " + l + " composed of " + t + " event types:");	    
+	 	 	
+	 	 /*** Create k random patterns of length l ***/
+		 Random random = new Random();
+		 for (int i=0; i<k; i++) {
+			 String pattern = "";
+			 for (int j=0; j<l; j++) {
+				int event_type = random.nextInt(t) + 1;
+				pattern += event_type + " ";
+			 }
+			 System.out.println(pattern);
+		}			 
+	 }
 }
